98b90fe74f6debe25076ba9955f1bbe5a91bb2eb
dstabenow <148902195+dstabenow@users.noreply.github.com>
2024-07-10 16:46:34 -0500

    Create export.sh

---
+++ export.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+read -p "Enter domain name: " domain
+if [[ -z "$domain" ]]; then
+  echo "Error: Please enter a domain name."
+  exit 1
+fi
+
+command="cli53 export --full --debug $domain > $domain.txt 2> $domain.err.log"
+eval "$command"
+if [[ $? -ne 0 ]]; then
+  echo "Error: cli53 command failed. See $domain.err.log for details."
+fi
+echo "Domain information exported to $domain.txt"
+echo "Debug log exported to $domain.err.log"
+
+removelines="python3 modify.py $domain.txt"
+eval "$removelines"
+if [[ $? -ne 0 ]]; then
+  echo "Error: remove_lines.py script failed."
+fi
+echo "NS and SOA records removed from $domain.txt"
