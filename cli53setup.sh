c1d80b52c48d999fb2a3ce49b34769d0ec0128b5
dstabenow <148902195+dstabenow@users.noreply.github.com>
2024-07-10 16:45:42 -0500

    Create cli53setup.sh

---
+++ cli53setup.sh
@@ -0,0 +1,4 @@
+wget -P /home/cloudshell-user https://github.com/barnybug/cli53/releases/download/0.8.22/cli53-linux-amd64
+sudo mv /home/cloudshell-user/cli53-linux-amd64 /usr/local/bin/cli53
+sudo chmod +x /usr/local/bin/cli53
+bash creds.sh
